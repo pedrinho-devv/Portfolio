772e91af43e9f627a2f1f0053affcebc645b0f9e
PedroAugusto <pedrodesousakiske@gmail.com>
2025-01-20 20:58:36 -0300

    sem o shiny

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Navbar from './Components/Navbar.jsx'
-import ShinyEffect from './Components/ShinyEffect.jsx'
-import Hero from './Components/Hero.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Navbar from "./Components/Navbar.jsx";
 
-createRoot(document.getElementById('root')).render(
+import Hero from "./Components/Hero.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbar />
-    <ShinyEffect/>
-    <Hero/>
-  </StrictMode>,
-)
+    <Hero />
+  </StrictMode>
+);
