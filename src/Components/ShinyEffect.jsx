c4781d30d386dfb780ad3b8fbd4d2bd1750d0423
PedroAugusto <pedrodesousakiske@gmail.com>
2025-01-20 21:10:28 -0300

    shiny1.0

--- src/Components/ShinyEffect.jsx
+++ src/Components/ShinyEffect.jsx
@@ -1,25 +1,23 @@
+const ShinyEffect = ( left, right, top, size = '200vw') => {
 
-const ShinyEffect = (left, right, top, size = '10vw') => {
+  const positionStyles = {
+    top: `${top}px`,
+    width: size,
+    height: size,
+    zIndex: -1,
+  };
 
-    const positionStyles = {
-        top:`${top}px`,
-        width:`${size}px`,
-        height:`${size}px`,
-        zIndex: -1
-    }
+  if (left !== undefined) {
+    positionStyles.left = `${left}px`;
+  }
 
-    if (left !== undefined) {
-        positionStyles.left = `${left}px`
-    }
-
-    if (right !== undefined) {
-        positionStyles.right = `${right}px`
-    }
-
+  if (right !== undefined) {
+    positionStyles.right = `${right}px`;
+  }
 
   return (
-    <div className='shiny-effect' style={positionStyles}></div>
-  )
+    <div className="shiny-effect" style={positionStyles}></div>
+  );
 }
 
-export default ShinyEffect
+export default ShinyEffect;
